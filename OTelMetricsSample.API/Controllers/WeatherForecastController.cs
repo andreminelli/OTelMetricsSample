f0ce152ff16e6261bd8e4e0cbbf0b5b23b3e1ba6
André Minelli (Take.net) <andre@take.net>
2023-04-03 14:51:29 -0300

    Add project files.

---
+++ OTelMetricsSample.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace OTelMetricsSample.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly Counter<int> _getCounter = MetricSource.Meter.CreateCounter<int>("get-requests");
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ OTelMetricsSample.API/MetricSource.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace OTelMetricsSample.API
+{
+    public static class MetricSource
+    {
+        public const string MeterName = "OTelMetricsSample.API";
+
+        internal static readonly Meter Meter = new(MeterName);
+    }
+}
