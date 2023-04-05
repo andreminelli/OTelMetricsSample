b2919883a4d2eba63deaf2d086ecce5a3e1883e8
André Minelli (Take.net) <andre@take.net>
2023-04-05 10:13:30 -0300

    Add Prometheus middleware to export metrics

--- OTelMetricsSample.API/Program.cs
+++ OTelMetricsSample.API/Program.cs
@@ -1,3 +1,6 @@
+using OTelMetricsSample.API;
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,8 +10,26 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseRouting();
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapMetrics();
+
+    Metrics.ConfigureMeterAdapter(options =>
+    {
+        options.InstrumentFilterPredicate = instrument =>
+        {
+            return instrument.Meter.Name == MetricSource.MeterName;
+        };
+    });
+
+    Metrics.SuppressDefaultMetrics(new SuppressDefaultMetricOptions
+    {
+        SuppressEventCounters = true,
+        SuppressDebugMetrics = true
+    });
+});
 
 app.MapControllers();
 
